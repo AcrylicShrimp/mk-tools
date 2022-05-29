ee246e86e142d88f64819a49ba3625ece3bc74e0
AcrylicShrimp <led789zxpp@naver.com>
2022-05-30 01:41:46 +0900

    feat: update the api usage.

--- scripts/entry.lua
+++ scripts/entry.lua
@@ -7,4 +7,48 @@ mk.Entity.build {
     }
 };
 
-local test_button = ui.createButton('test', 200, 60, 'Click me!')
+local test_button = ui.createButton('test', 100, 30, 'Click me!')
+test_button.button.transform.position = {
+    x = 0,
+    y = 100
+};
+
+local normal = mk.SpriteNinePatch.load("button")
+local hover = mk.SpriteNinePatch.load("button_hover")
+local down = mk.SpriteNinePatch.load("button_down")
+
+-- mk.Event.KeyDown.listen(function(event)
+--     print(event.key)
+-- end)
+
+-- mk.Event.KeyUp.listen(function(event)
+--     print(event.key)
+-- end)
+
+mk.Event.PointerEnter.listen(function()
+    test_button.button.nine_patch_renderer.nine_patch = hover
+end)
+
+mk.Event.PointerExit.listen(function()
+    test_button.button.nine_patch_renderer.nine_patch = normal
+end)
+
+mk.Event.PointerDown.listen(function(event)
+    if event.button ~= 'left' then
+        return
+    end
+
+    test_button.button.nine_patch_renderer.nine_patch = down
+end)
+
+mk.Event.PointerUp.listen(function(event)
+    if event.button ~= 'left' then
+        return
+    end
+
+    test_button.button.nine_patch_renderer.nine_patch = hover
+end)
+
+-- mk.Event.PointerMove.listen(function(event)
+--     print('pointer move', event.pointer_x, event.pointer_y)
+-- end)
--- scripts/ui.lua
+++ scripts/ui.lua
@@ -3,6 +3,9 @@ local table = {}
 table.createButton = function(name, width, height, text)
     local button = mk.Entity.build {
         name = name,
+        ui_element = {
+            order_index = 0
+        },
         nine_patch_renderer = {
             order = 0,
             shader = mk.Shader.load("nine-patch"),
@@ -16,15 +19,24 @@ table.createButton = function(name, width, height, text)
         transform = {
             parent = button.transform,
             position = {
-                x = width / 2 - 400,
-                y = height / 2 - 16
+                x = width / 2 - 100,
+                y = 8 - height / 2
+            },
+            scale = {
+                x = 0.25,
+                y = 0.25
             }
+        },
+        ui_element = {
+            is_interactible = false,
+            order_index = 1
         },
         glyph_renderer = {
             order = 1,
+            color = mk.Color.black(),
             shader = mk.Shader.load("glyph"),
             font = mk.Font.load("neodgm"),
-            font_size = 32,
+            font_size = 64,
             config = {
                 max_width = 800,
                 horizontal_align = "center"
@@ -33,7 +45,10 @@ table.createButton = function(name, width, height, text)
         }
     }
 
-    return {button, label}
+    return {
+        button = button,
+        label = label
+    }
 end
 
 return table
