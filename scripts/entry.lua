892de71a3db0729a5babd6da9f3d7e9301eed51f
AcrylicShrimp <led789zxpp@naver.com>
2022-05-30 03:28:38 +0900

    feat: update the api usage.

--- scripts/entry.lua
+++ scripts/entry.lua
@@ -25,30 +25,46 @@ local down = mk.SpriteNinePatch.load("button_down")
 --     print(event.key)
 -- end)
 
-mk.Event.PointerEnter.listen(function()
-    test_button.button.nine_patch_renderer.nine_patch = hover
-end)
+-- mk.Event.PointerEnter.listen(function()
+--     test_button.button.nine_patch_renderer.nine_patch = hover
+-- end)
 
-mk.Event.PointerExit.listen(function()
-    test_button.button.nine_patch_renderer.nine_patch = normal
-end)
+-- mk.Event.PointerExit.listen(function()
+--     test_button.button.nine_patch_renderer.nine_patch = normal
+-- end)
 
-mk.Event.PointerDown.listen(function(event)
-    if event.button ~= 'left' then
-        return
-    end
+-- mk.Event.PointerDown.listen(function(event)
+--     if event.button ~= 'left' then
+--         return
+--     end
 
-    test_button.button.nine_patch_renderer.nine_patch = down
-end)
+--     test_button.button.nine_patch_renderer.nine_patch = down
+-- end)
 
-mk.Event.PointerUp.listen(function(event)
-    if event.button ~= 'left' then
-        return
-    end
+-- mk.Event.PointerUp.listen(function(event)
+--     if event.button ~= 'left' then
+--         return
+--     end
 
-    test_button.button.nine_patch_renderer.nine_patch = hover
-end)
+--     test_button.button.nine_patch_renderer.nine_patch = hover
+-- end)
 
 -- mk.Event.PointerMove.listen(function(event)
 --     print('pointer move', event.pointer_x, event.pointer_y)
 -- end)
+
+test_button.button:listen("mouse-enter", function()
+    test_button.button.nine_patch_renderer.nine_patch = hover
+end)
+
+test_button.button:listen("mouse-exit", function()
+    test_button.button.nine_patch_renderer.nine_patch = normal
+end)
+
+test_button.button:listen("mouse-down", function(event)
+    test_button.button.nine_patch_renderer.nine_patch = down
+end)
+
+test_button.button:listen("mouse-up", function(event)
+    test_button.button.nine_patch_renderer.nine_patch = hover
+end)
